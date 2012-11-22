9a529d83dad50dc084695762d300c51a71af45c4
Aleksandr <nikulinaleksandr@gmail.com>
2012-11-22 20:42:18 +0400

    change routes for locales

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Golf::Application.routes.draw do
 
-
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
-
   devise_for :users
-
   mount Ckeditor::Engine => '/ckeditor'
 
-  scope :path => "(:locale)", :shallow_path => "(:locale)" do
+  scope "(:locale)", locale: /en|ru/ do
     root to: 'catalogs#index'
-    resource :catalogs
+    resources :catalogs
   end
 
 end
