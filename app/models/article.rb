7973597a7719c5f27f51ecb944bf5465272860e4
Aleksandr <nikulinaleksndr@gmail.com>
2012-11-22 13:29:53 +0400

    set default_locale to articles in rails_admin

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,10 @@
 class Article < ActiveRecord::Base
- attr_accessible :title, :body, :locale
- translates :title, :body
+ attr_accessible :title, :body, :article_translations
+ translates :title, :body do
+   validates :title, presence: true
+ end
+ has_many :article_translations
+ accepts_nested_attributes_for :article_translations
 
  class Translation
    attr_accessible :locale
--- app/models/article_translation.rb
+++ app/models/article_translation.rb
@@ -1,7 +1,9 @@
 class ArticleTranslation < ActiveRecord::Base
  
+ attr_accessible :title, :body, :locale, :article_id
  belongs_to :article
- 
+ after_initialize { self.locale ||= I18n.default_locale if new_record? }
+
  def locale_enum
    I18n.available_locales
  end
