2d72a5595504d6f1a1e364b8a72eda681e4a3585
Aleksandr <nikulinaleksandr@gmail.com>
2012-11-24 18:06:06 +0400

    changes in cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,13 +5,21 @@ class Cart
     @items = []
   end
 
-  def add_product(product, quantity)
+  def add_product(product, quantity=1)
     current_item = @items.find { |item| item.product == product }
     if current_item
       current_item.increment_quantity(quantity)
     else
       @items << CartItem.new(product, quantity)
     end
+  end
+
+  def price
+    @items.sum{ |item| item.price }
+  end
+
+  def quantity
+    @items.sum{ |item| item.quantity }
   end
 
 end
